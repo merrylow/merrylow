1b937677a7c4645770dc18c5be73893bb42c356e
Kelvin <agbakpeykelvin@gmail.com>
2025-01-28 20:24:47 +0000

    check 2

--- lib/api.ts
+++ lib/api.ts
@@ -1,6 +1,6 @@
 import { Product, Store } from "./definitions";
 
-const BASE_URL = process.env.NEXT_PUBLIC_WORDPRESS_SITE_URL;
+const BASE_URL = process.env.NEXT_PUBLIC_WORDPRESS_SITE_URL || "http://localhost:3000";
 
 
 export const API = {
@@ -9,29 +9,6 @@ export const API = {
      ORDERS: `${BASE_URL}/wp-json/wp/v2/orders`, 
      MENUS: `${BASE_URL}/wp-json/wp/v2/menus`,
 };
-
-
-// fetch products
-export async function fetchProducts() {
-     const response = await fetch('api/products');
-
-     if(!response.ok) {
-          throw new Error('Failed to fetch products');
-     } 
-
-     return response.json();
-}
-
-
-export async function fetchStores() {
-     const response = await fetch('api/products/stores');
-
-     if(!response.ok) {
-          throw new Error('Failed to fetch products');
-     } 
-
-     return response.json();
-}
 
 
 export async function fetchStoresAndProducts(): Promise<{ 
