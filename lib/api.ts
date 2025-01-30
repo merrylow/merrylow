1146908d9d7c415989a6784e063faf4bd5f79b41
Kelvin <agbakpeykelvin@gmail.com>
2025-01-30 20:59:45 +0000

    check 3

--- lib/api.ts
+++ lib/api.ts
@@ -1,15 +1,16 @@
 import { Product, Store } from "./definitions";
 
-export const BASE_URL = process.env.NODE_ENV === "production"
-     ? "https://merrylow.vercel.app"
-     : "http://localhost:3000"
-
 
 
 export async function fetchStoresAndProducts(): Promise<{ 
      stores: Store[]; 
      products: Product[] ;
 }> {
+     const BASE_URL = process.env.NODE_ENV === "production"
+     ? "https://merrylow.vercel.app"
+     : "http://localhost:3000";
+
+
      try {
           const storeResponse = await fetch(`${BASE_URL}/api/stores`);
           const stores: Store[] = await storeResponse.json();
