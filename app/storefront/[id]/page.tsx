11876b9e7eeb6a01c47fe0b7c79b4bcf4d4ebb99
merrylow <merrylow.ug@gmail.com>
2025-01-31 22:20:42 +0000

    fixed repeating menus issue

--- app/storefront/[id]/page.tsx
+++ app/storefront/[id]/page.tsx
@@ -6,7 +6,6 @@ import AddToCartButton from "@/components/ui/storefront/addToCartButton";
 
 
 export default async function StorefrontPage({ params }: { params: Promise<{ id: string }> }) {
-     // const storeId = parseInt(params.id, 10);
      const storeId = Number((await params).id);
      if (isNaN(storeId)) {
           return <div>Invalid Store ID</div>;
@@ -44,33 +43,28 @@ export default async function StorefrontPage({ params }: { params: Promise<{ id:
 
                <div className="w-[80%] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-10 mb-20 mx-auto">
                     {
-                         menuItems.map((product) => {
+                         menuItems.map((menuItem) => {
                               return (
-                                   <div key={product.id}>
-                                        <div className="space-y-6">
-                                             {menuItems.map((menuItem) => (
-                                                  <div key={menuItem.id} className="flex items-center h-28 space-x-3">
-                                                       <section className="w-[35%] h-28 flex items-center">
-                                                            <Image 
-                                                            src={menuItem.images[0].src} 
-                                                            alt={menuItem.name} 
-                                                            width={300}
-                                                            height={300} 
-                                                            style={{width: '100%', height: '100%'}}
-                                                            className="object-cover rounded-[6px]" />
-                                                       </section>
+                                   <div key={menuItem.id} className="flex items-center h-28 space-x-3">
+                                        <section className="w-[35%] h-28 flex items-center">
+                                             <Image 
+                                             src={menuItem.images[0].src} 
+                                             alt={menuItem.name} 
+                                             width={300}
+                                             height={300} 
+                                             style={{width: '100%', height: '100%'}}
+                                             className="object-cover rounded-[6px]" />
+                                        </section>
 
-                                                       <section className="h-[95%] flex flex-col justify-center space-y-4">
-                                                            <div>
-                                                                 <p className="text-[14.5px]">{menuItem.name}</p>
-                                                                 <p>c{menuItem.price}.00</p>
-                                                            </div>
-                                                            <AddToCartButton product={menuItem} />
-                                                       </section>
-                                                  </div>
-                                             ))}
-                                        </div>
+                                        <section className="h-[95%] flex flex-col justify-center space-y-4">
+                                             <div>
+                                                  <p className="text-[14.5px]">{menuItem.name}</p>
+                                                  <p>c{menuItem.price}.00</p>
+                                             </div>
+                                             <AddToCartButton product={menuItem} />
+                                        </section>
                                    </div>
+
                               )
                          })
                     }
